c12ba38a46668b0de25e885dc5a157fe090ff866
Torskzilla <>
2026-03-25 15:06:42 +0100

    Prevent clone tag from triggering on banned cards

--- src/tags.lua
+++ src/tags.lua
@@ -15,24 +15,28 @@ SMODS.Tag {
         if context.type == 'store_joker_create' and #G.jokers.cards > 0 then
             local owned_joker_keys = {}
             for i=1,#G.jokers.cards do
-                owned_joker_keys[#owned_joker_keys+1] = G.jokers.cards[i].config.center.key
+                if not G.GAME.banned_keys[G.jokers.cards[i].config.center.key] then
+                    owned_joker_keys[#owned_joker_keys+1] = G.jokers.cards[i].config.center.key
+                end
             end
 
-            local owned_joker = pseudorandom_element(owned_joker_keys, 'cod_clone_tag')
-            local card = SMODS.create_card {
-                set = "Joker",
-                key = owned_joker,
-                area = context.area,
-                key_append = "cod_clone_tag"
-            }
-            create_shop_card_ui(card, 'Joker', context.area)
-            card.states.visible = false
-            tag:yep('+', G.C.BLUE, function()
-                card:start_materialize()
-                return true
-            end)
-            tag.triggered = true
-            return card
+            if #owned_joker_keys > 0 then
+                local owned_joker = pseudorandom_element(owned_joker_keys, 'cod_clone_tag')
+                local card = SMODS.create_card {
+                    set = "Joker",
+                    key = owned_joker,
+                    area = context.area,
+                    key_append = "cod_clone_tag"
+                }
+                create_shop_card_ui(card, 'Joker', context.area)
+                card.states.visible = false
+                tag:yep('+', G.C.BLUE, function()
+                    card:start_materialize()
+                    return true
+                end)
+                tag.triggered = true
+                return card
+            end
         end
     end
 }
